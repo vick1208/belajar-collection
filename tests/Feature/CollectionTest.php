edaff9f10f53bcc5e3dbe8d6f8314bf4dbb75e82
Vicky AS <vickyhuang808@gmail.com>
2023-07-06 20:19:24 +0700

    lazy collection

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Data\Person;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\LazyCollection;
 use Tests\TestCase;
 
 use function PHPUnit\Framework\assertEquals;
@@ -394,7 +395,21 @@ class CollectionTest extends TestCase
     public function testReduce()
     {
         $col = collect([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-        $res = $col->reduce(fn($carry,$item)=>$carry+$item);
-        assertEquals(45,$res);
+        $res = $col->reduce(fn ($carry, $item) => $carry + $item);
+        assertEquals(45, $res);
+    }
+
+    public function testLazyCollect()
+    {
+        $collect = LazyCollection::make(function () {
+            $value = 0;
+            while (true) {
+                yield $value;
+                $value++;
+            }
+        });
+
+        $res = $collect->take(10);
+        assertEqualsCanonicalizing([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], $res->all());
     }
 }
